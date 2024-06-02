ab2bfb0943dc3328c3982293fdfd30b9d8ac0db5
macs1756 <kindrativmaksum312@gmail.com>
2024-06-02 15:53:38 +0300

    v15:53

--- src/components/OneTodo.tsx
+++ src/components/OneTodo.tsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { MdDragIndicator } from "react-icons/md";
 
 interface ItodoProps {
   description: string;
+  type: "execute" | "inProccess" | "success";
 }
 
-const Todo: React.FC<ItodoProps> = ({ description }) => {
-  return <div>{description}</div>;
+const renderColor = (type: string) => {
+  if (type === "execute") {
+    return "bg-[#583838]";
+  }
+
+  if (type === "inProccess") {
+    return "bg-[green]";
+  }
+
+  if (type === "success") {
+    return "bg-[#617e30]";
+  }
+};
+
+const Todo: React.FC<ItodoProps> = ({ description, type }) => {
+  return (
+    <div
+      className={
+        "cursor-pointer p-[10px] my-[10px] text-[14px] " + renderColor(type)
+      }
+    >
+      <div className="flex gap-[10px]">
+        <div>
+          <MdDragIndicator />
+        </div>
+        <div>{description}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
